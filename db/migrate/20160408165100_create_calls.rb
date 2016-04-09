4407682747589c2b2603085c02e153f368d6dde1
Dimitri Jorge <jorge.dimitri@gmail.com>
2016-04-09 12:37:17 +0100

    Drop comments

--- db/migrate/20160408165100_create_calls.rb
+++ db/migrate/20160408165100_create_calls.rb
@@ -7,22 +7,3 @@ class CreateCalls < ActiveRecord::Migration
     end
   end
 end
-
-# {"TotalCost"=>"0.00300",
-#  "EndTime"=>"2016-04-08 17:38:20",
-#  "Direction"=>"inbound",
-#  "HangupCause"=>"NORMAL_CLEARING",
-#  "From"=>"sip:caller160408140203@phone.plivo.com",
-#  "CallerName"=>"Caller",
-#  "BillDuration"=>"60",
-#  "BillRate"=>"0.00300",
-#  "To"=>"sip:sales160408161440@phone.plivo.com",
-#  "AnswerTime"=>"2016-04-08 17:38:12",
-#  "StartTime"=>"2016-04-08 17:38:11",
-#  "Duration"=>"9",
-#  "CallUUID"=>"70ec418c-8c80-4c14-ba81-97db81914b11",
-#  "SIP-H-To"=>"<sip:sales160408161440@phone.plivo.com>",
-#  "CallStatus"=>"completed",
-#  "Event"=>"Hangup",
-#  "controller"=>"webhooks",
-#  "action"=>"hangup"}
